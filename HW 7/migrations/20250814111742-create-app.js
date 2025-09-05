5b350aaef7bde27bf6c07b38c3d975c570decf3c
Toni Saab <antonii.saab@gmail.com>
2025-09-05 08:21:55 +0200

    Add HW 7

---
+++ HW 7/migrations/20250814111742-create-app.js
@@ -0,0 +1,31 @@
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Apps', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      size: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Apps');
+  }
+};
